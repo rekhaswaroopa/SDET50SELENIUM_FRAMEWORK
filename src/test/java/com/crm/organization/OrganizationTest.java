dd8fc9549cb3873b9b1c9ba0d2980bf8cbca9a91
rekhaswaroopa <38184109+rekhaswaroopa@users.noreply.github.com>
2022-05-11 17:44:59 +0530

    Update OrganizationTest.java

--- src/test/java/com/crm/organization/OrganizationTest.java
+++ src/test/java/com/crm/organization/OrganizationTest.java
@@ -3,7 +3,7 @@ package com.crm.organization;
 import org.testng.annotations.Test;
 
 public class OrganizationTest {
-	//hello
+	//hello bindhu
 	@Test(groups = {"smokeTest"})
 	public void createOrg()
 	{
